ffb6b3da583bd6ca8f3a1cb7f8f84e8d4d5a4c36
thoph <thoph@dev.ndex.net>
2023-05-27 14:02:33 +0700

    init unity dependency

--- Global.asax.cs
+++ Global.asax.cs
@@ -14,7 +14,8 @@ namespace WebNails.Admin
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+            AreaRegistration.RegisterAllAreas(); 
+            UnityConfig.RegisterComponents();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
