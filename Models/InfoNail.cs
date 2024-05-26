4c13f5aae7754bb842fd736fe7eb332d5631a769
thoph <thoph@dev.ndex.net>
2024-05-26 23:01:32 +0700

    admin

---
+++ Models/InfoNail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebNails.Admin.Models
+{
+    public class InfoNail
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string TextTell { get; set; }
+        public string HyperLinkTell { get; set; }
+        public string LinkBookingAppointment { get; set; }
+        public string GooglePlus { get; set; }
+        public string LinkGoogleMapAddress { get; set; }
+        public string LinkIFrameGoogleMap { get; set; }
+        public string BusinessHours { get; set; }
+        public string HomeAboutUs { get; set; }
+        public string AboutUs { get; set; }
+        public bool ShowCoupon { get; set; }
+        public JsonSocial Telegram { get; set; }
+        public JsonSocial Facebook { get; set; }
+        public JsonSocial Instagram { get; set; }
+        public JsonSocial Twitter { get; set; }
+        public JsonSocial Youtube { get; set; }
+    }
+}
